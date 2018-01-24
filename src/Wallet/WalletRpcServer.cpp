35ef28b9d48cb1137dbed57627ece1d22c389791
Adrian Herridge <editfmah@yahoo.co.uk>
2018-01-24 20:30:12 +0000

    add security to RPC layer
    add “—allow-extended-rpc” feature to allow access to wallet address, keys & to stop the service

--- src/Wallet/WalletRpcServer.cpp
+++ src/Wallet/WalletRpcServer.cpp
@@ -36,10 +36,12 @@ namespace Tools {
 
 const command_line::arg_descriptor<uint16_t> wallet_rpc_server::arg_rpc_bind_port = { "rpc-bind-port", "Starts wallet as rpc server for wallet operations, sets bind port for server", 0, true };
 const command_line::arg_descriptor<std::string> wallet_rpc_server::arg_rpc_bind_ip = { "rpc-bind-ip", "Specify ip to bind rpc server", "127.0.0.1" };
+const command_line::arg_descriptor<bool>      arg_allow_extended_rpc  = {"allow-extended-rpc", "Allow RPC access to the wallet address and view/spend keys", false};
 
 void wallet_rpc_server::init_options(boost::program_options::options_description& desc) {
   command_line::add_arg(desc, arg_rpc_bind_ip);
   command_line::add_arg(desc, arg_rpc_bind_port);
+  command_line::add_arg(desc, arg_allow_extended_rpc);
 }
 //------------------------------------------------------------------------------------------------------------------------------
 wallet_rpc_server::wallet_rpc_server(
@@ -78,6 +80,7 @@ void wallet_rpc_server::send_stop_signal() {
 bool wallet_rpc_server::handle_command_line(const boost::program_options::variables_map& vm) {
   m_bind_ip = command_line::get_arg(vm, arg_rpc_bind_ip);
   m_port = command_line::get_arg(vm, arg_rpc_bind_port);
+  m_allow_extended_rpc = command_line::get_arg(vm, arg_allow_extended_rpc);
   return true;
 }
 //------------------------------------------------------------------------------------------------------------------------------
@@ -108,6 +111,7 @@ void wallet_rpc_server::processRequest(const CryptoNote::HttpRequest& request, C
       { "get_payments", makeMemberMethod(&wallet_rpc_server::on_get_payments) },
       { "get_transfers", makeMemberMethod(&wallet_rpc_server::on_get_transfers) },
       { "get_height", makeMemberMethod(&wallet_rpc_server::on_get_height) },
+      // below are the restricted methods, use --enable-extended-rpc
       { "reset", makeMemberMethod(&wallet_rpc_server::on_reset) },
       { "stop_wallet", makeMemberMethod(&wallet_rpc_server::on_stop_wallet) },
       { "get_address", makeMemberMethod(&wallet_rpc_server::on_get_address) },
@@ -297,31 +301,37 @@ bool wallet_rpc_server::on_reset(const wallet_rpc::COMMAND_RPC_RESET::request& r
 
 //------------------------------------------------------------------------------------------------------------------------------
 bool wallet_rpc_server::on_stop_wallet(const wallet_rpc::COMMAND_RPC_STOP::request& req, wallet_rpc::COMMAND_RPC_STOP::response& res) {
-  try {
-    WalletHelper::storeWallet(m_wallet, m_walletFilename);
-  } catch (std::exception& e) {
-    throw JsonRpc::JsonRpcError(WALLET_RPC_ERROR_CODE_UNKNOWN_ERROR, std::string("Couldn't save wallet: ") + e.what());
+  
+  if(m_allow_extended_rpc) {
+    try {
+      WalletHelper::storeWallet(m_wallet, m_walletFilename);
+    } catch (std::exception& e) {
+      throw JsonRpc::JsonRpcError(WALLET_RPC_ERROR_CODE_UNKNOWN_ERROR, std::string("Couldn't save wallet: ") + e.what());
+    }
+
+    wallet_rpc_server::send_stop_signal();
   }
-
-  wallet_rpc_server::send_stop_signal();
-
   return true;
+  
 }
 
 bool wallet_rpc_server::on_get_address(const wallet_rpc::COMMAND_RPC_GET_ADDRESS::request& req, wallet_rpc::COMMAND_RPC_GET_ADDRESS::response& res) {
   
-  res.address = m_wallet.getAddress();
+  if(m_allow_extended_rpc) {
+    res.address = m_wallet.getAddress();
+  }
   return true;
 
 }
 
 bool wallet_rpc_server::on_view_keys(const wallet_rpc::COMMAND_RPC_VIEW_KEYS::request& req, wallet_rpc::COMMAND_RPC_VIEW_KEYS::response& res) {
   
-  AccountKeys keys;
-  m_wallet.getAccountKeys(keys);
-  res.view_key = Common::podToHex(keys.viewSecretKey);
-  res.spend_key = Common::podToHex(keys.spendSecretKey);
-
+  if(m_allow_extended_rpc) {
+    AccountKeys keys;
+    m_wallet.getAccountKeys(keys);
+    res.view_key = Common::podToHex(keys.viewSecretKey);
+    res.spend_key = Common::podToHex(keys.spendSecretKey);
+  }
   return true;
 
 }
