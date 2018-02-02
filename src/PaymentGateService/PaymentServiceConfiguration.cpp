7a966b40f3e967974982321c3ccc1a50265d355d
amon <amon@nandynarwhals.org>
2018-02-02 10:05:19 +0800

    Replace the default 0.0.0.0 RPC bind port for walletd with 127.0.0.1.

--- src/PaymentGateService/PaymentServiceConfiguration.cpp
+++ src/PaymentGateService/PaymentServiceConfiguration.cpp
@@ -46,7 +46,7 @@ Configuration::Configuration() {
 
 void Configuration::initOptions(boost::program_options::options_description& desc) {
   desc.add_options()
-      ("bind-address", po::value<std::string>()->default_value("0.0.0.0"), "payment service bind address")
+      ("bind-address", po::value<std::string>()->default_value("127.0.0.1"), "payment service bind address")
       ("bind-port", po::value<uint16_t>()->default_value(8070), "payment service bind port")
       ("container-file,w", po::value<std::string>(), "container file")
       ("container-password,p", po::value<std::string>(), "container password")
