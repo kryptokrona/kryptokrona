4fe68e2242b73efb50a47340a83729027f28015c
RockSteady <34389545+RocksteadyTC@users.noreply.github.com>
2018-02-11 22:43:00 -0600

    Remove False Synchronization Notice
    Replace with information that's actually useful and accurate.

--- src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
+++ src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
@@ -581,14 +581,13 @@ bool CryptoNoteProtocolHandler::request_missing_objects(CryptoNoteConnectionCont
 bool CryptoNoteProtocolHandler::on_connection_synchronized() {
   bool val_expected = false;
   if (m_synchronized.compare_exchange_strong(val_expected, true)) {
-    logger(Logging::INFO) << ENDL << "**********************************************************************" << ENDL
-      << "You are now synchronized with the network. You may now start simplewallet." << ENDL
+    logger(Logging::INFO) << ENDL
       << ENDL
-      << "Please note, that the blockchain will be saved only after you quit the daemon with \"exit\" command or if you use \"save\" command." << ENDL
-      << "Otherwise, you will possibly need to synchronize the blockchain again." << ENDL
+      << "Always exit TurtleCoind and Simplewallet with the \"exit\" command." << ENDL
+      << "If you do not exit properly, you may lose your blockchain and wallet data." << ENDL
       << ENDL
       << "Use \"help\" command to see the list of available commands." << ENDL
-      << "**********************************************************************";
+      << "Use \"export_keys\" command to display your keys for restoring a corrupted wallet.";
 
     m_observerManager.notify(&ICryptoNoteProtocolObserver::blockchainSynchronized, m_core.getTopBlockIndex());
   }
