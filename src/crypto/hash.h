dd1b69573c10daf052c48af89f7c7ccdcf4c716b
Suml Noether <whydo+youlookmeup@quiet.org>
2018-03-24 13:45:29 +0000

    Remove all references to the old cn_slow_hash and replace with version specific cn_slow_hash_v6.

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -56,16 +56,11 @@ namespace Crypto {
   private:
 
     void *data;
-    friend inline void cn_slow_hash(cn_context &, const void *, size_t, Hash &);
     friend inline void cn_slow_hash_v6(cn_context &, const void *, size_t, Hash &);
     friend inline void cn_slow_hash_v7(cn_context &, const void *, size_t, Hash &);
     friend inline void cn_lite_slow_hash_v0(cn_context &, const void *, size_t, Hash &);
     friend inline void cn_lite_slow_hash_v1(cn_context &, const void *, size_t, Hash &);
   };
-
-  inline void cn_slow_hash(cn_context &context, const void *data, size_t length, Hash &hash) {
-    (*cn_slow_hash_f)(context.data, data, length, reinterpret_cast<void *>(&hash), 0, 0);
-  }
 
   inline void cn_slow_hash_v6(cn_context &context, const void *data, size_t length, Hash &hash) {
     (*cn_slow_hash_f)(context.data, data, length, reinterpret_cast<void *>(&hash), 0, 0);
