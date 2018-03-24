b8fad5311755eb8aaec29c76d585cb0537590295
Suml Noether <37329252+sumlnoether@users.noreply.github.com>
2018-03-25 02:09:19 +0800

    Added attribution to the Monero project.

--- src/crypto/cryptonight-variants.h
+++ src/crypto/cryptonight-variants.h
@@ -1,4 +1,5 @@
 // Copyright (c) 2017-2018, The Turtlecoin Developers
+// Copyright (c) 2018, The Monero Project
 // Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
 //
 // This file is part of Bytecoin.
