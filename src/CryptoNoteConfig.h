cc6c7a5a2cc490beb36b379b597e7c339f829f44
RockSteady <34389545+RocksteadyTC@users.noreply.github.com>
2018-04-01 23:23:44 +0000

    0.4.2
    reverting future block validation change

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -31,7 +31,7 @@ const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000;
 const size_t   CRYPTONOTE_MAX_SAFE_TX_SIZE                   = 115000;
 const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 3914525;
 const uint32_t CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 40;
-const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 7 * 30;
+const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
 
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 
