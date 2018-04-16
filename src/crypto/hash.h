91149734bb23251494023c5c7825cbeba3094ec4
Rob Hensley <hensleyrob@gmail.com>
2018-04-16 14:54:09 -0400

    Turtlecoin -> TurtleCoin

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018, The Turtlecoin developers
+// Copyright (c) 2017-2018, The TurtleCoin developers
 // Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
 //
 // This file is part of Bytecoin.
