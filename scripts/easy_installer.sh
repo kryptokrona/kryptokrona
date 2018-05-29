232bae11d2f64c53f57fd49e3ad2545a7a83c2be
RockSteady <34389545+RocksteadyTC@users.noreply.github.com>
2018-05-29 05:32:02 +0000

    ubuntu 18 LTS update

--- scripts/easy_installer.sh
+++ scripts/easy_installer.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 # rocksteady, turtlecoin developers 2017-2018
-# use this installer to clone-and-compile turtlecoin in one line
-# supports Ubuntu 16 LTS
+# use this installer to clone-and-compile turtlecoin
+# supports Ubuntu 18 LTS
 
 sudo apt-get update
-yes "" | sudo apt-get install build-essential python-dev gcc-4.9 g++-4.9 git cmake libboost1.58-all-dev librocksdb-dev
-export CXXFLAGS="-std=gnu++11"
+sudo apt-get install build-essential python-dev gcc g++ git cmake libboost-all-dev librocksdb-dev -y
 git clone https://github.com/turtlecoin/turtlecoin
 cd turtlecoin
 mkdir build && cd $_
