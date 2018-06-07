cc2f2571c1fdf3402edbf119c10c3d161778b537
ZedPea <zp+github@zacharypalmer.co.uk>
2018-06-07 02:38:22 +0100

    Add fork schedule and upgrade notification to status command

--- src/Rpc/RpcServer.cpp
+++ src/Rpc/RpcServer.cpp
@@ -492,7 +492,18 @@ bool RpcServer::on_get_info(const COMMAND_RPC_GET_INFO::request& req, COMMAND_RP
   res.grey_peerlist_size = m_p2p.getPeerlistManager().get_gray_peers_count();
   res.last_known_block_index = std::max(static_cast<uint32_t>(1), m_protocol.getObservedHeight()) - 1;
   res.network_height = std::max(static_cast<uint32_t>(1), m_protocol.getBlockchainHeight());
-  res.upgrade_height = CryptoNote::parameters::UPGRADE_HEIGHT_CURRENT;
+
+  res.upgrade_height = std::numeric_limits<uint64_t>::max();
+
+  for (auto height : CryptoNote::parameters::FORK_HEIGHTS) {
+      if (res.network_height < height) {
+          res.upgrade_height = height;
+          break;
+      }
+  }
+
+  res.supported_height = CryptoNote::parameters::FORK_HEIGHTS[CryptoNote::parameters::CURRENT_FORK_INDEX];
+
   res.hashrate = (uint32_t)round(res.difficulty / CryptoNote::parameters::DIFFICULTY_TARGET);
   res.synced = ((uint64_t)res.height == (uint64_t)res.network_height);
   res.testnet = m_core.getCurrency().isTestnet();
