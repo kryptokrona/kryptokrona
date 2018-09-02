01e0a67a6483f184df1ee115addd75a1913f8f3a
Brandon Lehmann <brandonlehmann@gmail.com>
2018-09-02 12:20:23 -0400

    Bumps the P2P minimum and current version to the new minimum. Version 2 existed starting with 9eba74839082c43475cf53cab73b9af7f151619a that is being built into 0.8.3. This commit cuts older nodes off the network.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -215,8 +215,8 @@ const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
 
 // P2P Network Configuration Section - This defines our current P2P network version
 // and the minimum version for communication between nodes
-const uint8_t  P2P_CURRENT_VERSION                           = 2;
-const uint8_t  P2P_MINIMUM_VERSION                           = 1;
+const uint8_t  P2P_CURRENT_VERSION                           = 3;
+const uint8_t  P2P_MINIMUM_VERSION                           = 2;
 
 const size_t   P2P_CONNECTION_MAX_WRITE_BUFFER_SIZE          = 32 * 1024 * 1024; // 32 MB
 const uint32_t P2P_DEFAULT_CONNECTIONS_COUNT                 = 8;
