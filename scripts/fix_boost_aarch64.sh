b2b8d7692200512de0a9e6a35d44fca870f68a65
Brandon <brandonlehmann@gmail.com>
2018-09-13 23:17:55 -0400

    Update fix_boost_aarch64.sh

--- scripts/fix_boost_aarch64.sh
+++ scripts/fix_boost_aarch64.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+# This file is meant to be ran during the BOOST build process using a 
+# cross-compile toolchain for AARCH64
+
 sed -i 's/using gcc/using gcc : aarch64 : aarch64-linux-gnu-g++/g' project-config.jam
