77d57dbffa938806531fe3fe51bbc876167d7a8c
Brandon Lehmann <brandonlehmann@gmail.com>
2018-09-15 00:33:43 -0400

    Let's try to have the Transaction Pool Cleaner clean out transactions with invalid mixins

---
+++ src/CryptoNoteCore/Mixins.h
@@ -0,0 +1,102 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+// Copyright (c) 2018, The TurtleCoin Developers
+//
+// Please see the included LICENSE file for more information.
+
+#pragma once
+#include "CachedTransaction.h"
+#include "TransactionApi.h"
+#include <config/CryptoNoteConfig.h>
+
+namespace CryptoNote
+{
+  class Mixins
+  {
+    public:
+      static std::tuple<bool, std::string> validate(std::vector<CachedTransaction> transactions, uint32_t height)
+      {
+        uint64_t minMixin = 0;
+        uint64_t maxMixin = std::numeric_limits<uint64_t>::max();
+
+        /* We now limit the mixin allowed in a transaction. However, there have been
+         some transactions outside these limits in the past, so we need to only
+         enforce this on new blocks, otherwise wouldn't be able to sync the chain */
+
+        /* We also need to ensure that the mixin enforced is for the limit that
+         was correct when the block was formed - i.e. if 0 mixin was allowed at
+         block 100, but is no longer allowed - we should still validate block 100 */
+        if (height >= CryptoNote::parameters::MIXIN_LIMITS_V3_HEIGHT)
+        {
+            minMixin = CryptoNote::parameters::MINIMUM_MIXIN_V3;
+            maxMixin = CryptoNote::parameters::MAXIMUM_MIXIN_V3;
+        }
+        else if (height >= CryptoNote::parameters::MIXIN_LIMITS_V2_HEIGHT)
+        {
+            minMixin = CryptoNote::parameters::MINIMUM_MIXIN_V2;
+            maxMixin = CryptoNote::parameters::MAXIMUM_MIXIN_V2;
+        }
+        else if (height >= CryptoNote::parameters::MIXIN_LIMITS_V1_HEIGHT)
+        {
+            minMixin = CryptoNote::parameters::MINIMUM_MIXIN_V1;
+            maxMixin = CryptoNote::parameters::MAXIMUM_MIXIN_V1;
+        }
+
+        for (const auto& transaction : transactions)
+        {
+            bool success;
+            std::string error;
+
+            std::tie(success, error) = validate(transaction, minMixin, maxMixin);
+            if (!success)
+            {
+                return std::make_tuple(success, error);
+            }
+        }
+
+        return std::make_tuple(true, std::string());
+      }
+
+      static std::tuple<bool, std::string> validate(const CachedTransaction& transaction, uint64_t minMixin, uint64_t maxMixin)
+      {
+        uint64_t ringSize = 1;
+
+        const auto tx = createTransaction(transaction.getTransaction());
+
+        for (size_t i = 0; i < tx->getInputCount(); ++i) {
+          if (tx->getInputType(i) != TransactionTypes::InputType::Key) {
+            continue;
+          }
+
+          KeyInput input;
+          tx->getInput(i, input);
+          const uint64_t currentRingSize = input.outputIndexes.size();
+          if (currentRingSize > ringSize) {
+              ringSize = currentRingSize;
+          }
+        }
+
+        /* Ring size = mixin + 1 - your transaction plus the others you mix with */
+        const uint64_t mixin = ringSize - 1;
+
+        if (mixin > maxMixin) {
+          std::stringstream str;
+
+          str << "Transaction " << transaction.getTransactionHash()
+            << " is not valid. Reason: transaction mixin is too large (" << mixin
+            << "). Maximum mixin allowed is " << maxMixin;
+
+          return std::make_tuple(false, str.str());
+        } else if (mixin < minMixin) {
+          std::stringstream str;
+
+          str << "Transaction " << transaction.getTransactionHash()
+            << " is not valid. Reason: transaction mixin is too small (" << mixin
+            << "). Minimum mixin allowed is " << minMixin;
+
+          return std::make_tuple(false, str.str());
+        }
+
+        return std::make_tuple(true, std::string());
+      }
+  };
+}
