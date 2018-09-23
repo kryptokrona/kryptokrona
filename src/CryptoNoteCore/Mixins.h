1482d18fe1e4389e4596fe434492ad18bb4c56ac
Brandon Lehmann <brandonlehmann@gmail.com>
2018-09-23 16:37:52 -0400

    Moving the validateMixins from WalletService into the common Mixins.h

--- src/CryptoNoteCore/Mixins.h
+++ src/CryptoNoteCore/Mixins.h
@@ -6,6 +6,7 @@
 #pragma once
 #include "CachedTransaction.h"
 #include "TransactionApi.h"
+#include "Wallet/WalletErrors.h"
 #include <config/CryptoNoteConfig.h>
 
 namespace CryptoNote
@@ -13,18 +14,60 @@ namespace CryptoNote
   class Mixins
   {
     public:
+
+      /* This method is used by WalletService to determine if the mixin amount is correct
+         for the current block height */
+      static std::tuple<bool, std::string, std::error_code> validate(const uint32_t mixin, const uint32_t height)
+      {
+        uint64_t minMixin = 0;
+        uint64_t maxMixin = std::numeric_limits<uint64_t>::max();
+        
+        if (height >= CryptoNote::parameters::MIXIN_LIMITS_V3_HEIGHT)
+        {
+          minMixin = CryptoNote::parameters::MINIMUM_MIXIN_V3;
+          maxMixin = CryptoNote::parameters::MAXIMUM_MIXIN_V3;
+        }
+        else if (height >= CryptoNote::parameters::MIXIN_LIMITS_V2_HEIGHT)
+        {
+          minMixin = CryptoNote::parameters::MINIMUM_MIXIN_V2;
+          maxMixin = CryptoNote::parameters::MAXIMUM_MIXIN_V2;
+        }
+        else if (height >= CryptoNote::parameters::MIXIN_LIMITS_V1_HEIGHT)
+        {
+          minMixin = CryptoNote::parameters::MINIMUM_MIXIN_V1;
+          maxMixin = CryptoNote::parameters::MAXIMUM_MIXIN_V1;
+        }
+        
+        std::stringstream str;
+
+        if (mixin < minMixin)
+        {
+          str << "Mixin of " << mixin << " under minimum mixin threshold of " << minMixin;
+          return std::make_tuple(false, str.str(), make_error_code(CryptoNote::error::MIXIN_BELOW_THRESHOLD));
+        }
+        else if (mixin > maxMixin)
+        {
+          str << "Mixin of " << mixin << " above maximum mixin threshold of " << maxMixin;
+          return std::make_tuple(false, str.str(), make_error_code(CryptoNote::error::MIXIN_ABOVE_THRESHOLD));
+        }
+        
+        return std::make_tuple(true, std::string(), std::error_code());
+      }
+    
+      /* This method is commonly used by the node to determine if the transactions in the vector have
+         the correct mixin (anonymity) as defined by the current rules */
       static std::tuple<bool, std::string> validate(std::vector<CachedTransaction> transactions, uint32_t height)
       {
         uint64_t minMixin = 0;
         uint64_t maxMixin = std::numeric_limits<uint64_t>::max();
 
         /* We now limit the mixin allowed in a transaction. However, there have been
-         some transactions outside these limits in the past, so we need to only
-         enforce this on new blocks, otherwise wouldn't be able to sync the chain */
+           some transactions outside these limits in the past, so we need to only
+           enforce this on new blocks, otherwise wouldn't be able to sync the chain */
 
         /* We also need to ensure that the mixin enforced is for the limit that
-         was correct when the block was formed - i.e. if 0 mixin was allowed at
-         block 100, but is no longer allowed - we should still validate block 100 */
+           was correct when the block was formed - i.e. if 0 mixin was allowed at
+           block 100, but is no longer allowed - we should still validate block 100 */
         if (height >= CryptoNote::parameters::MIXIN_LIMITS_V3_HEIGHT)
         {
             minMixin = CryptoNote::parameters::MINIMUM_MIXIN_V3;
@@ -56,6 +99,8 @@ namespace CryptoNote
         return std::make_tuple(true, std::string());
       }
 
+      /* This method is commonly used by the node to determine if the transaction has
+         the correct mixin (anonymity) as defined by the current rules */
       static std::tuple<bool, std::string> validate(const CachedTransaction& transaction, uint64_t minMixin, uint64_t maxMixin)
       {
         uint64_t ringSize = 1;
@@ -78,17 +123,15 @@ namespace CryptoNote
         /* Ring size = mixin + 1 - your transaction plus the others you mix with */
         const uint64_t mixin = ringSize - 1;
 
+        std::stringstream str;
+        
         if (mixin > maxMixin) {
-          std::stringstream str;
-
           str << "Transaction " << transaction.getTransactionHash()
             << " is not valid. Reason: transaction mixin is too large (" << mixin
             << "). Maximum mixin allowed is " << maxMixin;
 
           return std::make_tuple(false, str.str());
         } else if (mixin < minMixin) {
-          std::stringstream str;
-
           str << "Transaction " << transaction.getTransactionHash()
             << " is not valid. Reason: transaction mixin is too small (" << mixin
             << "). Minimum mixin allowed is " << minMixin;
