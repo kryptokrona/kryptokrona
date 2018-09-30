c4cc0767eae9ad27e1dc775c242c54d4eec73f0b
Brandon Lehmann <brandonlehmann@gmail.com>
2018-09-30 17:06:26 -0400

    Quick Crypto Test tool for multi hashing data for testing

---
+++ src/CryptoTest/main.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+//
+// Please see the included LICENSE file for more information.
+
+#include <iostream>
+
+#include "CryptoNote.h"
+#include "CryptoTypes.h"
+#include "Common/StringTools.h"
+#include "crypto/crypto.h"
+
+using namespace Crypto;
+using namespace CryptoNote;
+
+int main(int argc, char** argv) {
+  try {
+    if (argc != 2)
+    {
+      throw std::runtime_error("Must supply data to hash");
+    }
+
+    std::string input = argv[1];
+
+    const BinaryArray& rawData = Common::fromHex(input);
+
+    std::cout << "\n\nInput: " << input << "\n\n";
+
+    Hash hash = Hash();
+
+    cn_fast_hash(rawData.data(), rawData.size(), hash);
+    std::cout << "Cryptonight Fast Hash: " << Common::podToHex(hash) << "\n";
+
+    cn_slow_hash_v6(rawData.data(), rawData.size(), hash);
+    std::cout << "Cryptonight v0: " << Common::podToHex(hash) << "\n";
+
+    cn_slow_hash_v7(rawData.data(), rawData.size(), hash);
+    std::cout << "Cryptonight v1: " << Common::podToHex(hash) << "\n";
+
+    cn_lite_slow_hash_v0(rawData.data(), rawData.size(), hash);
+    std::cout << "Cryptonight Lite v0: " << Common::podToHex(hash) << "\n";
+
+    cn_lite_slow_hash_v1(rawData.data(), rawData.size(), hash);
+    std::cout << "Cryptonight Lite v1: " << Common::podToHex(hash) << "\n";
+  }
+  catch (std::exception& e)
+  {
+    std::cout << "Something went terribly wrong...\n" << e.what() << "\n\n";
+  }
+
+  return 0;
+}
