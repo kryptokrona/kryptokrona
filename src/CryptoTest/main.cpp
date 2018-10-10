620ad8136643e6f7a34d44a5c10ca2870f0e50e4
Brandon Lehmann <brandonlehmann@gmail.com>
2018-10-10 11:48:15 -0400

    Add Crypto Performance Tests to CryptoTest binary

--- src/CryptoTest/main.cpp
+++ src/CryptoTest/main.cpp
@@ -3,11 +3,14 @@
 // Please see the included LICENSE file for more information.
 
 #include <iostream>
+#include <chrono>
 
 #include "CryptoNote.h"
 #include "CryptoTypes.h"
 #include "Common/StringTools.h"
 #include "crypto/crypto.h"
+
+#define PERFORMANCE_ITERATIONS  1000
 
 using namespace Crypto;
 using namespace CryptoNote;
@@ -28,22 +31,40 @@ int main(int argc, char** argv) {
     Hash hash = Hash();
 
     cn_fast_hash(rawData.data(), rawData.size(), hash);
-    std::cout << "Cryptonight Fast Hash: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+    std::cout << "cn_fast_hash: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
 
     cn_slow_hash_v0(rawData.data(), rawData.size(), hash);
-    std::cout << "Cryptonight v0: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+    std::cout << "cn_slow_hash_v0: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
 
     if (rawData.size() >= 43)
     {
       cn_slow_hash_v1(rawData.data(), rawData.size(), hash);
-      std::cout << "Cryptonight v1: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      std::cout << "cn_slow_hash_v1: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
 
       cn_lite_slow_hash_v0(rawData.data(), rawData.size(), hash);
-      std::cout << "Cryptonight Lite v0: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      std::cout << "cn_lite_slow_hash_v0: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
 
       cn_lite_slow_hash_v1(rawData.data(), rawData.size(), hash);
-      std::cout << "Cryptonight Lite v1: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
+      std::cout << "cn_lite_slow_hash_v1: " << Common::toHex(&hash, sizeof(Hash)) << "\n";
     }
+
+    std::cout <<  "\nPerformance Tests: Please wait, this may take a while depending on your system...\n\n";
+
+    auto startTimer = std::chrono::high_resolution_clock::now();
+    for (auto i = 0; i < PERFORMANCE_ITERATIONS; i++)
+    {
+      cn_slow_hash_v0(rawData.data(), rawData.size(), hash);
+    }
+    auto elapsedTime = std::chrono::high_resolution_clock::now() - startTimer;
+    std::cout << "cn_slow_hash_v0: " << (PERFORMANCE_ITERATIONS / std::chrono::duration_cast<std::chrono::seconds>(elapsedTime).count()) << " H/s\n";
+
+    startTimer = std::chrono::high_resolution_clock::now();
+    for (auto i = 0; i < PERFORMANCE_ITERATIONS; i++)
+    {
+      cn_lite_slow_hash_v0(rawData.data(), rawData.size(), hash);
+    }
+    elapsedTime = std::chrono::high_resolution_clock::now() - startTimer;
+    std::cout << "cn_lite_slow_hash_v0: " << (PERFORMANCE_ITERATIONS / std::chrono::duration_cast<std::chrono::seconds>(elapsedTime).count()) << " H/s\n";
   }
   catch (std::exception& e)
   {
