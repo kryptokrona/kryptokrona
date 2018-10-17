3366ea9656767397f76d49dc5eed187becefc284
Brandon Lehmann <brandonlehmann@gmail.com>
2018-10-16 22:38:55 -0400

    Fixes https://github.com/turtlecoin/turtlecoin/issues/547
    If you want to swap view keys (as in swap everything in a container to a new set of keys) create a new container.

--- src/WalletService/PaymentServiceJsonRpcMessages.cpp
+++ src/WalletService/PaymentServiceJsonRpcMessages.cpp
@@ -26,21 +26,7 @@ void Export::Response::serialize(CryptoNote::ISerializer& serializer) {
 }
 
 void Reset::Request::serialize(CryptoNote::ISerializer& serializer) {
-  bool hasKey = serializer(viewSecretKey, "viewSecretKey");
-
-  bool hasScanHeight = serializer(scanHeight, "scanHeight");
-  bool hasNewAddress = serializer(newAddress, "newAddress");
-
-  /* Can't specify both that it is a new address, and a height to begin
-     scanning from */
-  if (hasNewAddress && hasScanHeight) {
-    throw RequestSerializationError();
-  }
-
-  /* It's not a reset if you're not resetting :thinking: */
-  if (!hasKey && hasNewAddress) {
-    throw RequestSerializationError();
-  };
+  serializer(scanHeight, "scanHeight");
 }
 
 void Reset::Response::serialize(CryptoNote::ISerializer& serializer) {
