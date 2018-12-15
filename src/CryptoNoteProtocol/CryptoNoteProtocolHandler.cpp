3290c0f521147e29c652849a4a052c1717fa5e2f
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2018-12-15 07:22:07 +0000

    Forking tweaks

--- src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
+++ src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
@@ -605,7 +605,8 @@ bool CryptoNoteProtocolHandler::request_missing_objects(CryptoNoteConnectionCont
     requestMissingPoolTransactions(context);
 
     context.m_state = CryptoNoteConnectionContext::state_normal;
-    logger(Logging::INFO, Logging::BRIGHT_GREEN) << context << "Successfully synchronized with the TurtleCoin Network.";
+    logger(Logging::INFO, Logging::BRIGHT_GREEN) << context << "Successfully synchronized with the "
+                                                 << CryptoNote::CRYPTONOTE_NAME << " Network.";
     on_connection_synchronized();
   }
   return true;
