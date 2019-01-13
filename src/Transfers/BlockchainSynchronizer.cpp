1d381a061c3a24529809222d5198bdc088d167d8
thinkpol <21112772+thinkpol2@users.noreply.github.com>
2019-01-13 08:28:12 -0600

    Fix fall-through in BlockchainSynchronizer.cpp

--- src/Transfers/BlockchainSynchronizer.cpp
+++ src/Transfers/BlockchainSynchronizer.cpp
@@ -538,6 +538,7 @@ void BlockchainSynchronizer::processBlocks(GetBlocksResponse& response) {
       if (m_node.getKnownBlockCount() != m_node.getLocalBlockCount()) {
         m_logger(DEBUGGING) << "Blockchain updated, resume blockchain synchronization";
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        break;
       } else {
         break;
       }
