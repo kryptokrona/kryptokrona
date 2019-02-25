9e0ce4f470a3bfbced28f0d9c0c1f798c92793f9
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-02-25 05:53:32 +0000

    Bump p2p version

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -230,8 +230,8 @@ const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
 
 // P2P Network Configuration Section - This defines our current P2P network version
 // and the minimum version for communication between nodes
-const uint8_t  P2P_CURRENT_VERSION                           = 4;
-const uint8_t  P2P_MINIMUM_VERSION                           = 3;
+const uint8_t  P2P_CURRENT_VERSION                           = 5;
+const uint8_t  P2P_MINIMUM_VERSION                           = 4;
 
 // This defines the minimum P2P version required for lite blocks propogation
 const uint8_t P2P_LITE_BLOCKS_PROPOGATION_VERSION            = 4;
