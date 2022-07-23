a44be415f01a3a1271264830599b3a59dab2de25
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-07-23 20:07:21 +0200

    Refactoring config package

--- src/config/cryptonote_config.h
+++ src/config/cryptonote_config.h
@@ -13,7 +13,7 @@
 #include <initializer_list>
 #include <boost/uuid/uuid.hpp>
 
-namespace CryptoNote {
+namespace cryptonote {
 namespace parameters {
 
 const uint64_t DIFFICULTY_TARGET                             = 10; // seconds
