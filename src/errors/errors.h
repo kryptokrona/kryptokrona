f76e94274abcbc4339f1c4e364330c337c6a9581
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-07-26 11:47:27 +0200

    Refactoring wallet backend package

--- src/errors/errors.h
+++ src/errors/errors.h
@@ -185,12 +185,12 @@ enum ErrorCode
     UNEXPECTED_FEE = 45,
 
     /* Value given is negative, but must be >= 0
-       NOTE: Not used in WalletBackend, only here to maintain API compatibility
+       NOTE: Not used in wallet_backend, only here to maintain API compatibility
        with turtlecoin-wallet-backend-js */
     NEGATIVE_VALUE_GIVEN = 46,
 
     /* Key is not 64 char hex 
-       NOTE: Not used in WalletBackend, only here to maintain API compatibility
+       NOTE: Not used in wallet_backend, only here to maintain API compatibility
        with turtlecoin-wallet-backend-js */
     INVALID_KEY_FORMAT = 47,
 
@@ -201,7 +201,7 @@ enum ErrorCode
     HASH_INVALID = 49,
 
     /* Number is a float, not an integer
-       NOTE: Not used in WalletBackend, only here to maintain API compatibility
+       NOTE: Not used in wallet_backend, only here to maintain API compatibility
        with turtlecoin-wallet-backend-js */
     NON_INTEGER_GIVEN = 50,
 };
