ca83df4842f7cea9f444ca2b856a17a664dc46a0
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-21 17:49:55 +0200

    Updating ansible script

--- ansible/ansible.sh
+++ ansible/ansible.sh
@@ -2,11 +2,11 @@
 # this file is intended for manually deploy from your local computer and not in the GitHub Actions workflow
 
 # checks if keypair doesn't exist
-if [ ! -f ~/.ssh/id_hugin_api ]; then
-    ssh-keygen -t rsa -N "" -f ~/.ssh/id_hugin_api
+if [ ! -f ~/.ssh/id_kryptokrona ]; then
+    ssh-keygen -t rsa -N "" -f ~/.ssh/id_kryptokrona
     eval "$(ssh-agent -s)" &>/dev/null
-    ssh-add ~/.ssh/id_hugin_api &>/dev/null
-    ssh-copy-id -i ~/.ssh/id_hugin_api.pub root@api.hugin.chat
+    ssh-add ~/.ssh/id_kryptokrona &>/dev/null
+    ssh-copy-id -i ~/.ssh/id_kryptokrona.pub root@xkr.mjovanc.com
 fi
 
 ansible-playbook provision_vps.yml -i prod.inventory --ask-vault-pass
