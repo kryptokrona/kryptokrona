b1db35cb6de9ae20dd36c4c6a14cb30e7cb54768
mjovanc <mjovanc@icloud.com>
2024-03-27 10:26:23 +0100

    Removing old files

--- scripts/fix_boost_aarch64.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This file is meant to be ran during the BOOST build process using a 
-# cross-compile toolchain for AARCH64
-
-echo "Setting up user compiler gcc-aarch64: ${HOME}/toolchain/gcc-arm-8.2-2018.08-x86_64-aarch64-linux-gnu/bin/aarch64-linux-gnu-g++"
-echo "using gcc : aarch64 : ${HOME}/toolchain/gcc-arm-8.2-2018.08-x86_64-aarch64-linux-gnu/bin/aarch64-linux-gnu-g++ ; " >> tools/build/v2/user-config.jam
--- scripts/internal-checkpoints-tool/README.md
+++
@@ -1,3 +0,0 @@
-To use this tool, run `npm install`
-
-Drop the checkpoints.csv file in the folder with this script then run `node checkpoints.js`
