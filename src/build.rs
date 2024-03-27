fb4085f8fdccb1fde7a63924aa7de0dc6471faa3
mjovanc <mjovanc@icloud.com>
2024-03-27 09:55:44 +0100

    Removing wallet_api in CMakeLists.txt

--- src/build.rs
+++ src/build.rs
@@ -2,12 +2,19 @@
 
 use std::process::Command;
 use std::env;
+use std::path::{Path, PathBuf};
 
 fn main() {
+    let cmake_lists_path = PathBuf::from(env::current_dir().unwrap())
+        .join("../../CMakeLists.txt")
+        .canonicalize()
+        .expect("Failed to calculate path to CMakeLists.txt");
+
     let build_dir = env::var("OUT_DIR").unwrap();
+
     Command::new("cmake")
         .arg("-S")
-        .arg("../../CMakeLists.txt")
+        .arg(&cmake_lists_path)
         .arg("-B")
         .arg(&build_dir)
         .status()
