6e7690a881e714d52d31c11453975ab06f8073c4
mjovanc <mjovanc@icloud.com>
2024-04-03 20:42:45 +0200

    Updating build script for crypto

--- src/crypto/build.rs
+++ src/crypto/build.rs
@@ -2,12 +2,20 @@ extern crate cc;
 
 fn main() {
     let mut build = cc::Build::new();
+    let tool = build.get_compiler();
 
-    build.flag("-std=c99");
+    if tool.is_like_gnu() || tool.is_like_clang() {
+        build.flag_if_supported("-std=c99");
+        build
+            .flag_if_supported("-msse4.1")
+            .flag_if_supported("-maes");
+    }
+
     build.warnings(false);
     build.include("../common/cpp");
 
-    build.file("c/aesb.c")
+    build
+        .file("c/aesb.c")
         .file("c/blake256.c")
         .file("c/crypto-ops-data.c")
         .file("c/crypto-ops.c")
