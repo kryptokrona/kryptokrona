ccd797cfcc09e1a206dfdc755ac2582d877a61a3
fr00t <fruktstav@gmail.com>
2024-11-01 13:32:00 +0100

    Update wallet_service.h

--- src/wallet_service/wallet_service.h
+++ src/wallet_service/wallet_service.h
@@ -93,6 +93,7 @@ namespace payment_service
         std::error_code createIntegratedAddress(const std::string &address, const std::string &paymentId, std::string &integratedAddress);
         std::error_code getFeeInfo(std::string &address, uint32_t &amount);
         uint64_t getDefaultMixin() const;
+        bool validateAddress(const std::string &address);
 
     private:
         void refresh();
